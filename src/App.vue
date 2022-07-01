9f334c95fe46cb07d903be88eea613c456e57428
Becker Mathie <bmathie@mpr.org>
2022-07-01 15:32:09 -0500

    vue todo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import TodoListVue from "./components/TodoList.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    TodoListVue,
+  },
+};
+</script>
+<template>
+  <TodoListVue />
+</template>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,47 @@
+<script>
+import sampleTodos from "../data/todos.json";
+
+export default {
+  data() {
+    return {
+      todos: sampleTodos,
+      newTodoTitle: "",
+      filtered: false,
+    };
+  },
+  methods: {
+    addTodo() {
+      const newTodo = {
+        title: this.newTodoTitle,
+        completed: false,
+      };
+      this.todos.push(newTodo);
+    },
+    removeTodo(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
+    },
+  },
+  computed: {
+    filteredTodos() {
+      return this.todos.filter((todo) => !todo.completed);
+    },
+  },
+};
+</script>
+<template>
+  <h2>Todo List!</h2>
+  <span>
+    <input v-model="newTodoTitle" /><button @click="addTodo">
+      Add Todo
+    </button>
+    <input type="checkbox" v-model="filtered" />
+    <span>Hide completed</span>
+  </span>
+  <div v-for="todo in filtered ? filteredTodos : todos">
+    <span>
+      <input v-model="todo.completed" type="checkbox" />
+      <span>{{ todo.title }}</span>
+      <button @click="removeTodo(todo)">Remove</button>
+    </span>
+  </div>
+</template>
